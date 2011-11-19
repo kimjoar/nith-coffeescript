ee9c9ffafbbe5758ea2e51d3b122033bdc4c479f
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2011-11-19 20:27:32 +0100

    More info + running code examples

---
+++ eval-coffeescript.js
@@ -0,0 +1,58 @@
+!function(context) {
+
+  var compileAndEval = function(el) {
+    var source = prepare(el)
+      , compiledJS = ''
+      ;
+
+      try {
+        compiledJS = CoffeeScript.compile(source, { bare: true });
+        evalJS(compiledJS);
+      } catch (error) {
+        alert(error);
+      }
+  };
+
+  var prepare = function(el) {
+    var element = $(el)
+      , html
+      ;
+
+    element.children(".run").remove();
+    html = element.html().replace(/<\/?[^>]+>/gi, '');
+    html = html.replace('&gt;', '>', 'gi');
+    addRunButton(el, false);
+
+    return html;
+  };
+
+  var evalJS = function(code) {
+    try {
+      eval(code);
+    } catch(error) {
+      alert(error);
+    }
+  };
+
+  var addRunButton = function(parent, bindEvent) {
+    if ($(parent).children('.run').length > 0) {
+      return;
+    }
+
+    var el = $(parent).append('<a class="run coffeescript">Run code</a>');
+
+    if (typeof bindEvent == "undefined" || bindEvent) {
+      $(el).click(function(e) {
+        e.preventDefault();
+        compileAndEval(el);
+      });
+    }
+  };
+
+  window.loadConsolesOn = function(selector) {
+    $(selector).each(function(index, element) {
+      addRunButton(element);
+    });
+  };
+
+}(this);
